fb87cdbf5cf250a3d40cb708aecb504a314c4fdc
feiger <feiger>
2017-06-07 16:52:03 +0800

    upload index.js

--- js/index.js
+++ js/index.js
@@ -40,39 +40,39 @@ angular.module('app').config(['$provide', function($provide){
 angular.module('app').config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
 	$stateProvider.state('main', {
 		url: '/main',
-		templateUrl : "../view/main.html",
+		templateUrl : "/view/main.html",
 		controller: 'mainCtrl'
 	}).state('position', {
 		url : '/position/:id',
-		templateUrl : '../view/position.html',
+		templateUrl : '/view/position.html',
 		controller: 'positionCtrl'
 	}).state('company', {
 		url : '/company/:id',
-		templateUrl : '../view/company.html',
+		templateUrl : '/view/company.html',
 		controller: 'companyCtrl'
 	}).state('search', {
 		url : '/search/',
-		templateUrl : '../view/search.html',
+		templateUrl : '/view/search.html',
 		controller: 'searchCtrl'
 	}).state('login', {
 		url : '/login/',
-		templateUrl : '../view/login.html',
+		templateUrl : '/view/login.html',
 		controller: 'loginCtrl'
 	}).state('register', {
 		url : '/register/',
-		templateUrl : '../view/register.html',
+		templateUrl : '/view/register.html',
 		controller: 'registerCtrl'
 	}).state('favorite', {
 		url : '/favorite/',
-		templateUrl : '../view/favorite.html',
+		templateUrl : '/view/favorite.html',
 		controller: 'favoriteCtrl'
 	}).state('post', {
 		url : '/post/',
-		templateUrl : '../view/post.html',
+		templateUrl : '/view/post.html',
 		controller: 'postCtrl'
 	}).state('me', {
 		url : '/me/',
-		templateUrl : '../view/me.html',
+		templateUrl : '/view/me.html',
 		controller: 'meCtrl'
 	});
 
